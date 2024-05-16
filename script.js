eac6fc9cb10ba4971bf3185080b03faa51bd9583
jveliquette <juliaveliquette@yahoo.com>
2024-05-16 15:04:46 -0700

    added about section, functions, and styling

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ window.addEventListener("load", function() {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 })
+
+
+/* EXPAND PARAGRAPH */
+function toggleParagraph() {
+    var paragraph = document.getElementById("expandable-paragraph");
+    var readButton = document.getElementById("read-btn");
+    if (paragraph.style.maxHeight === "0px" || paragraph.style.maxHeight === "") {
+      paragraph.style.maxHeight = paragraph.scrollHeight + "px"; // Show the paragraph
+      readButton.style.display = "none"; // Hide the "Read" button
+    } else {
+      paragraph.style.maxHeight = "0"; // Hide the paragraph
+      readButton.style.display = "inline-block"; // Show the "Read" button
+    }
+  }
