3ac3dc375788533e552d71d376a1d46bb33b3b84
jveliquette <juliaveliquette@yahoo.com>
2024-05-17 19:48:45 -0700

    formatting and organizing of files

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
 'use strict';
 
+
 /* PRELOAD */
 /* LOADING WILL END AFTER DOCUMENT IS LOADED */
-
 const preloader = document.querySelector("[data-preload]");
 
 window.addEventListener("load", function() {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 })
-
-
-/* EXPAND PARAGRAPH */
-// function toggleParagraph() {
-//     var paragraph = document.getElementById("expandable-paragraph");
-//     var readButton = document.getElementById("read-btn");
-//     if (paragraph.style.maxHeight === "0px" || paragraph.style.maxHeight === "") {
-//       paragraph.style.maxHeight = paragraph.scrollHeight + "px"; // Show the paragraph
-//       readButton.style.display = "none"; // Hide the "Read" button
-//     } else {
-//       paragraph.style.maxHeight = "0"; // Hide the paragraph
-//       readButton.style.display = "inline-block"; // Show the "Read" button
-//     }
-//   }
 
 
 /* IMAGE SLIDER */
@@ -46,20 +32,7 @@ const imageWrapper = document.querySelector('.image-wrapper');
   }
 
 
-/* EXPAND PLANT MENU */
-// function toggleMenu() {
-//   var menu = document.getElementById("expandable-menu");
-//   var menuButton = document.getElementById("menu-btn");
-//   if (menu.style.maxHeight === "0px" || menu.style.maxHeight === "") {
-//     menu.style.maxHeight = menu.scrollHeight + "px"; // Expand the menu
-//     menuButton.style.display = "none";
-//   } else {
-//     menu.style.maxHeight = "0"; // Collapse the menu
-//     menuButton.style.display = "inline-block";
-//   }
-// }
-
-
+/* EXPAND PARAGRAPH */
 function toggleParagraph() {
   var paragraph = document.getElementById("expandable-paragraph");
   var readButton = document.getElementById("read-btn");
@@ -67,6 +40,8 @@ function toggleParagraph() {
   readButton.style.display = paragraph.classList.contains("expanded") ? "none" : "inline-block";
 }
 
+
+/* EXPAND MENU */
 function toggleMenu() {
   var menu = document.getElementById("expandable-menu");
   var menuButton = document.getElementById("menu-btn");
